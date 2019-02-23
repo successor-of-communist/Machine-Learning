ac2289e903311631c97cb06cbf71f374ad0f1666
xue <13277996909@163.com>
2019-02-23 10:07:12 +0800

    add

--- main.py
+++ main.py
@@ -7,35 +7,6 @@ from mpi4py import MPI
 import math
 import time
 
-def splitdata(datafile,P,unum):
-    #spliteddata=np.empty((P,rnum+1,4),int)
-    upnum=unum/P+1;
-    data=[0 for i in range(unum+1)]
-    datarank=[0 for i in range(unum+1)]
-    newrow=[0 for i in range(P)]
-    rpnum=[0 for i in range(P)]
-    for i in range(unum):
-        rand=random.randint(0,P-1)
-        while newrow[rand]>=upnum:
-            rand=random.randint(0,P-1)
-        else:
-            data[i]=rand
-            datarank[i]=newrow[rand]
-            newrow[rand]+=1
-    fi=open(datafile,'r')
-    for line in fi:
-        arr=line.split()
-        tmp=int(arr[0].strip())
-        rpnum[data[tmp]]+=1
-        afi=open('p'+str(data[tmp]),'a')
-        afi.write(line.strip()+'\t'+str(datarank[tmp])+'\n')
-        afi.close()
-    fi.close()
-    testnum=0
-    for i in range(P):
-        testnum+=rpnum[i]-int(rpnum[i]*0.9)
-    return testnum
-
 def getscore(up,vp):
     ret=np.dot(up,vp)
     if ret<1:
@@ -56,7 +27,19 @@ def validate(st,ed,data,up,vp):
 
     return rmse
 
-def dsadmm(up,vp,v_ave,unum,inum,rnum,comm,rank,size,k,P,start,testnum):
+def dsadmm(comm,rank,size,P,start,testnum,pdata,pnum):
+
+    k=40
+    unum=943
+    inum=1682
+    rnum=100000
+    up=np.random.random(size=(k+1,unum/P+1))
+    vp=np.random.random(size=(k+1,inum+1))
+    up/=math.sqrt(k)
+    vp/=math.sqrt(k)
+    v_ave=vp.copy()
+    v_ave.dtype=vp.dtype
+
     sita=np.zeros((k+1,inum+1),dtype=np.float)
     alpha=0.002
     beta=0.7
@@ -65,16 +48,15 @@ def dsadmm(up,vp,v_ave,unum,inum,rnum,comm,rank,size,k,P,start,testnum):
     lambda1=0.05
     lambda2=0.05
     pdata=np.empty([rnum+1,4],dtype=int)
-    pnum=0
-    fi=open('p'+str(rank),'r')
-    for line in fi:
-        arr=line.split()
-        pdata[pnum][0]=int(arr[0].strip())
-        pdata[pnum][1]=int(arr[1].strip())
-        pdata[pnum][2]=int(arr[2].strip())
-        pdata[pnum][3]=int(arr[3].strip())
-        pnum+=1
-    fi.close()
+    #fi=open('p'+str(rank),'r')
+    #for line in fi:
+    #    arr=line.split()
+    #    pdata[pnum][0]=int(arr[0].strip())
+    #    pdata[pnum][1]=int(arr[1].strip())
+    #    pdata[pnum][2]=int(arr[2].strip())
+    #    pdata[pnum][3]=int(arr[3].strip())
+    #    pnum+=1
+    #fi.close()
     trainpnum=int(pnum*0.9)
     for step in range(1):
         for l in range(trainpnum):
@@ -118,21 +100,41 @@ if __name__=='__main__':
     comm=MPI.COMM_WORLD
     rank=comm.Get_rank()
     size=comm.Get_size()
-    k=40
-    unum=943
-    inum=1682
-    rnum=100000
-    P=4
-    up=np.random.random(size=(k+1,unum/P+1))
-    vp=np.random.random(size=(k+1,inum+1))
-    up/=math.sqrt(k)
-    vp/=math.sqrt(k)
-    v_ave=vp.copy()
-    v_ave.dtype=vp.dtype
     testnum=0
     #if rank==0:
         #testnum=splitdata('./u.data',P,unum)
-    dsadmm(up,vp,v_ave,unum,inum,rnum,comm,rank,size,k,P,start,testnum=10000)
+    splitdata=np.empty((P,rnum+1,4),int)
+    upnum=unum/P+1;
+    data=[0 for i in range(unum+1)]
+    datarank=[0 for i in range(unum+1)]
+    newrow=[0 for i in range(P)]
+    rpnum=[0 for i in range(P)]
+    for i in range(unum):
+        rand=random.randint(0,P-1)
+        while newrow[rand]>=upnum:
+            rand=random.randint(0,P-1)
+        else:
+            data[i]=rand
+            datarank[i]=newrow[rand]
+            newrow[rand]+=1
+    fi=open(datafile,'r')
+    for line in fi:
+        arr=line.split()
+        uid=int(arr[0].strip())
+        iid=int(arr[0].strip())
+        rate=int(arr[0].strip())
+        tmp=data[uid]
+        splitdata[tmp][rpnum[tmp]]=[uid,iid,rate,datarank[uid]]
+        rpnum[tmp]+=1
+        #afi=open('p'+str(data[tmp]),'a')
+        #afi.write(line.strip()+'\t'+str(datarank[tmp])+'\n')
+        #afi.close()
+    fi.close()
+    testnum=0
+    for i in range(P):
+        testnum+=rpnum[i]-int(rpnum[i]*0.9)
+    splitdata=comm.bcast(splitdata,root=0)
+    dsadmm(comm,rank,size,P,start,testnum,splitdata[rank],rpnum[rank])
     end=time.clock()
     if rank==0:
         print(end-start)
